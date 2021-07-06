e39d557d3dbfa5d1870bc12907146a4b7586e365
DevJunior21 <junior-wmw@hotmail.com>
2021-07-06 15:31:58 -0400

    trabalhando com elementos

---
+++ TrabalhandoComElementos/script.js
@@ -0,0 +1,6 @@
+let x = document.getElementById('elementoOndeVoceEsta')
+let pai = x.parentElement
+pai.style.color = 'red'
+
+let primogenito = document.getElementById('primeiroFilhoDoFilho')
+primogenito.innerText = 'Olá Mundo'
